70312855c7084323a54513d29d8c822fe4e146b7
dexbiobot <zodpixel@gmail.com>
2021-08-29 13:04:40 +0500

    commit

--- index.js
+++ index.js
@@ -14,5 +14,11 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
 
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
 client.login(token);
